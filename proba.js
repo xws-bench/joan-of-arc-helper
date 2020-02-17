8456581fa0117a74775da52e57166f10d35ab192
Denis Wings <deniswings@free.fr>
2020-02-17 22:55:32 +0100

    again

--- proba.js
+++ proba.js
@@ -709,14 +709,14 @@ $( document ).ready(function() {
         $(this).addClass("selected");
         let img=$(this).attr("src").split('.')[0];
         trouveperso(1);
-        $(".hexa .hex8").css("fill","url(#"+img.substr(3)+")");
+        $(".hexa .hex8").css("fill","url(#"+img.substr(4)+")");
     });
     $(".img-circle2").click(function() {
         $(".img-circle2").removeClass("selected");
         $(this).addClass("selected");
         let img=$(this).attr("src").split('.')[0];
         trouveperso(2);
-        $(".hexa .hex9").css("fill","url(#"+img.substr(3)+")");
+        $(".hexa .hex9").css("fill","url(#"+img.substr(4)+")");
     });
 
 });
